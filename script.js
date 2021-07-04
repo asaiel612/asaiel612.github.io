99da6af8902b929d6ad421a53d451ede80c81bbd
asaiel612 <82271868+asaiel612@users.noreply.github.com>
2021-07-04 12:56:15 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+var speakWord = "Hello Yaakov ";
+		console.log(speakWord );
+
+        var speakWord = "Good Bye John ";
+		console.log(speakWord );
+
+        var speakWord = "Good Bye Jen ";
+		console.log(speakWord );
+
+        var speakWord = "Good Bye Jason ";
+		console.log(speakWord );
+
+        var speakWord = "Hello Paul";
+		console.log(speakWord );
+
+        var speakWord = "Hello Frank ";
+		console.log(speakWord );
+
+        var speakWord = "Hello Larry ";
+		console.log(speakWord );
+
+        var speakWord = "Hello Paula ";
+		console.log(speakWord );
+
+        var speakWord = "Hello Laura ";
+		console.log(speakWord );
+
+        var speakWord = "Good Bye Jim ";
+		console.log(speakWord );
